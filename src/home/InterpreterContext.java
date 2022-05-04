835ddbe9481c2be38588717182e1927b374f9d0a
AkashdeepArjun <wadhwaarjun007@gmail.com>
2022-05-04 02:46:19 -0700

    class as helper which interprets and does pretty much all interpretations

---
+++ src/home/InterpreterContext.java
@@ -0,0 +1,15 @@
+package home;
+public class InterpreterContext{
+
+        public String getBinaryFormat(int number){
+            return Integer.toBinaryString(number);
+        }
+        public String getHexaFormat(int number){
+
+            return Integer.toHexString(number);
+        }
+
+    
+    
+
+}
